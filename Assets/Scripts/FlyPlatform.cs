17908bbc0f779fc9737fd58905b8214af3eea494
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2021-11-25 20:33:32 +1200

    Добавила платформу

--- Assets/Scripts/FlyPlatform.cs
+++ Assets/Scripts/FlyPlatform.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class FlyPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Transform[] points;
+    public float speed = 1f;
+    int i = 1;
+    
     void Start()
     {
-        
+        transform.position = new Vector3(points[0].position.x, points[0].position.y, transform.position.z);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+
+            if (transform.position == points[i].position)
+            {
+                if (i < points.Length - 1)
+                    i++;
+                else
+                    i = 0;
+            }
+        }
     }
+
 }
