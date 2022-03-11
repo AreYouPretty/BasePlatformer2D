9bee04a103a46cb660e29ba8f6bbcecedc654161
Anastasia <anastasiaparcutova@gmail.com>
2022-03-11 04:56:46 +0300

    В скрипте Menu написан Start

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,8 +6,30 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Button[] lvls;
+
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("Lvls"))
+        {
+            for (int i = 0; i < lvls.Length - 1; i++)
+            {
+                if (i <= PlayerPrefs.GetInt("Lvls"))
+                {
+                    lvls[i].interactable = true;
+                }
+                else
+                    lvls[i].interactable = false;
+            }
+        }
+    }
     public void OpenScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void DelKeys()
+    {
+        
     }
 }
